2c44ab525db86b9ae4299c92d06ef948e3b4fd8a
Harshit <Harshitdubey1907@gmail.com>
2025-12-24 16:13:51 +0530

    TOTAL SYSTEM REBUILD - CLEAN UTF8

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,3 @@
 import * as admin from 'firebase-admin';
-
-// This checks if Firebase is already initialized (by index.ts)
-// If not, it uses the environment variable we added to Railway
-if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '{}');
-  
-  if (serviceAccount.project_id) {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount)
-    });
-  }
-}
-
 export const db = admin.firestore();
 export const auth = admin.auth();
